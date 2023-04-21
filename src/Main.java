48d67734df1fe71183748cce030c5cc391772daf
jonathan <jonathan_sm@mail.ru>
2023-04-21 09:10:32 +0300

    домашка номер 4-1 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,91 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("домашка 4");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+
+
     }
-}
+    public static void task1() {
+        System.out.println("Задача 1");
+        for ( int i = 0; i < 10; i = i + 1 ){
+            System.out.println("Итерация цикла " + i);
+        }
+        }
+    public static void task2() {
+        System.out.println("Задача 2");
+        for ( int i = 9 ; i >= 1; i = i - 1 ){
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        for (int i = 1; i <= 17; i = i +1) {
+            if ( i % 2 == 0)
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i = i - 1 ) {
+                System.out.println("Итерация цикла " + i);
+        }
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        for (int i = 1904; i < 2096; i = i + 4) {
+            System.out.println("Високосный год " + i);
+        }
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println("последовательность чисел " + i);
+        }
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println("последовательность чисел " + i);
+        }
+    }
+    public static void task8() {
+        System.out.println("Задача 8");
+        int salary = 29000;
+        double total = 0;
+        for (int i = 0; i <= 12; i++) {
+          total =total +salary;
+            System.out.println("Месяц  "+ i + " сумма накоплений равна " + total +" рублей" );
+        }
+    }
+    public static void task9() {
+        System.out.println("Задача 9");
+        int salary = 29000;
+        double total = 0;
+        int salaryWithPenny = salary * 100;
+        int totalWithPenny = 0;
+        for (int i = 0; i < 12; i++) {
+            totalWithPenny = totalWithPenny + totalWithPenny / 100;
+            totalWithPenny = totalWithPenny + salaryWithPenny;
+
+            total = totalWithPenny / 100.;
+            System.out.println("Месяц  " + i + " сумма накоплений равна " + total + " рублей");
+        }
+    }
+    public static void task10() {
+        System.out.println("Задача 10");
+        for (int i = 1; i <= 10; i++) {
+            int num = 2;
+            int result = num * i;
+            System.out.println(num + " * " + i + " = " + result);
+
+        }
+        }
+    }
